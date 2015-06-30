0820a78565ea5fd5d7ee796257b1aae0b15d93bd
freshrebel <arno_deckx@hotmail.com>
2015-06-30 14:37:19 +0200

    Add Login class (session var)

---
+++ CVOApp/CVOApp/Models/Login.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CVOApp.Models
+{
+    public static class Login
+    {
+        public static int LoginSession
+        {
+            get
+            {
+                object value = HttpContext.Current.Session["LoginSession"];
+                if (value != null)
+                {
+                    return Convert.ToInt32(value);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                HttpContext.Current.Session["LoginSession"] = value;
+            }
+        }
+    }
+}
